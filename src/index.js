6a0e043efd0549677cd49a2c90d0d4c4d66899f9
gkorbakis <gkorbakis@epu.ntua.gr>
2023-07-17 12:18:23 +0300

    Keycloak integration

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {ReactKeycloakProvider} from "@react-keycloak/web";
+import my_keycloak from "./Keycloak";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ReactKeycloakProvider authClient={my_keycloak} initOptions={{onLoad: 'login-required'}}>
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    </ReactKeycloakProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
