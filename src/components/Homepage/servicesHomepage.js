ca7622edbbe69227db608d2fb86ec68c7cc1d44a
gkorbakis <gkorbakis@epu.ntua.gr>
2023-07-17 12:02:24 +0300

    servicesHomepage.js: Some updates

--- src/components/Homepage/servicesHomepage.js
+++ src/components/Homepage/servicesHomepage.js
@@ -1,20 +1,20 @@
-import DocumentScannerIcon from '@mui/icons-material/DocumentScanner';
-import EnergySavingsLeafIcon from '@mui/icons-material/EnergySavingsLeaf';
+import ViewTimelineIcon from '@mui/icons-material/ViewTimeline';
+import SettingsSuggestIcon from '@mui/icons-material/SettingsSuggest';
 
 export const servicesHomepage = [
     {
-        'id': '0',
-        'title': 'UC7 dashboard first',
-        'description': 'A description of the first UC7 dashboard functionality.',
-        'icon': <DocumentScannerIcon style={{color: 'white', fontSize: '80px'}}/>,
+        'id': 'load_profile',
+        'title': 'Load Profile',
+        'description': 'Obtain the load profile for the smart meter\'s load pattern for the upcoming day by using clustering techniques',
+        'icon': <ViewTimelineIcon style={{color: 'white', fontSize: '80px'}}/>,
         'image': 'https://img.freepik.com/premium-photo/energy-efficiency-rating-buildings-sustainable-developmentbusinessman-pointing-energy-efficiency-rating-chart-house-icon-concept-performance-efficiency-environmental-conservation_43780-1198.jpg?w=1380',
         'link': ''
     },
     {
-        'id': '1',
-        'title': 'UC7 dashboard first',
-        'description': 'A description of the second UC7 dashboard functionality.',
-        'icon': <EnergySavingsLeafIcon style={{color: 'white', fontSize: '80px'}}/>,
+        'id': 'clusters_profile',
+        'title': 'Clusters Profile',
+        'description': 'Generate cluster profiles based on algorithm\'s outcomes and provide recommendations tailored to each cluster\'s characteristics.',
+        'icon': <SettingsSuggestIcon style={{color: 'white', fontSize: '80px'}}/>,
         'image': 'https://img.freepik.com/free-photo/green-energy-with-hand-holding-environmental-light-bulb-background_53876-124630.jpg?w=1380&t=st=1667896857~exp=1667897457~hmac=d2ab8c6bda8023155370418b931c534cb4778108938718024c807da8e922e0e4',
         'link': ''
     },
