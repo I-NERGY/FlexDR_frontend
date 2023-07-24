a7f0793ebd1c55febe5149e0b49878923f23fb22
gkorbakis <gkorbakis@epu.ntua.gr>
2023-07-24 10:56:43 +0300

    HomepageItemFullWidth.js: Some fixes

--- src/components/Homepage/HomepageItemFullWidth.js
+++ src/components/Homepage/HomepageItemFullWidth.js
@@ -14,7 +14,7 @@ const HomepageItemFullWidth = ({title, description, link, icon, image, index}) =
     let navigate = useNavigate();
 
     return (
-        <Grid container sx={{position: 'relative', minHeight: '46vh', maxHeight: '48vh'}}>
+        <Grid container sx={{position: 'relative', border: '1px solid red', mixHeight: '100%'}}>
             {link && <Avatar className={'linkAvatar'} onClick={() => navigate(link)}>
                 <InsertLinkIcon className={'serviceCategoryLinkIcon'}/>
             </Avatar>}
@@ -22,22 +22,22 @@ const HomepageItemFullWidth = ({title, description, link, icon, image, index}) =
             <Grid item className={(index % 2) ? 'serviceCategoryDescriptionEven' : 'serviceCategoryDescriptionOdd'}
                   xs={12} md={6}
                   order={(index % 2) ? {xs: 2, md: 2} : {xs: 2, md: 1}}
-                // sx={{height: {md: '100%', xs: '100%', zIndex: 2}}}
+                  sx={{height: {md: '600px', xs: '600px', zIndex: 2}}}
                   display={'flex'} alignItems={'center'}>
-                <Container maxWidth={'md'} sx={{p: 5}}>
+                <Container maxWidth={'md'} sx={{p: 3}}>
                     <Box display={'flex'} justifyContent={'center'} alignItems={'center'}>
                         {icon}
-                        <Typography variant={'h3'} color={'white'} align={'center'}>{title}</Typography>
+                        <Typography variant={'h4'} color={'white'} align={'center'}>{title}</Typography>
                     </Box>
                     <Divider color={'#fff'} sx={{width: '30%', mx: 'auto', height: '1px', mt: 5}}/>
-                    <Typography variant={'h4'} color={'white'} align={'center'}
+                    <Typography variant={'h5'} color={'white'} align={'center'}
                                 sx={{my: 3, px: 1, fontWeight: '100'}}>{description}</Typography>
                     <Divider color={'#fff'} sx={{width: '30%', mx: 'auto', height: '1px', mt: 3}}/>
                 </Container>
             </Grid>
-            <Grid item xs={12} md={6} sx={{overflow: 'hidden'}}
+            <Grid item xs={12} md={6} sx={{height: {md: '600px', xs: '600px', overflow: 'hidden'}}}
                   order={(index % 2) ? {xs: 1, md: 1} : {xs: 1, md: 2}}>
-                <img style={{height: '100%', width: '100%', objectFit: 'cover', maxHeight: '48vh'}}
+                <img style={{height: '100%', width: '100%', objectFit: 'cover'}}
                      src={image} className={'homepageServiceImage'}
                      alt=""/>
             </Grid>
