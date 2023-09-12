a8be5a8471a891fe72a62b02c71b63fc6530f49a
gkorbakis <gkorbakis@epu.ntua.gr>
2023-09-12 17:17:23 +0300

    SmartMetersInspection.js, Data.js: Editing recommendation feature added

--- src/components/SmartMetersInspection/Data.js
+++ src/components/SmartMetersInspection/Data.js
@@ -1,38 +1,147 @@
+import {useState} from "react";
 import {useTheme} from "@mui/material/styles";
 
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
 
 import {LineChart} from "./LineChart";
 
-const Data = ({cluster}) => {
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+
+const style = {
+    position: 'absolute',
+    top: '30%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '50%',
+    minWidth: '350px',
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 2,
+};
+
+const Data = ({cluster, handleSaveRecommendation, editModal, setEditModal}) => {
     const theme = useTheme()
 
+
+    const [recommendationUpdatedName, setRecommendationUpdatedName] = useState('')
+    const [recommendationUpdatedDescription, setRecommendationUpdatedDescription] = useState('')
+    const [recommendationUpdatedDetails, setRecommendationUpdatedDetails] = useState('')
+
+    const handleOpenModal = recommendation => {
+        setEditModal(true)
+        setRecommendationUpdatedName(recommendation.name)
+        setRecommendationUpdatedDescription(recommendation.description)
+        setRecommendationUpdatedDetails(recommendation.details)
+    };
+
+    const handleCloseModal = () => setEditModal(false);
+
+    const handleFieldChange = (attribute, value) => {
+        attribute === 'name' ? setRecommendationUpdatedName(value) :
+            attribute === 'description' ? setRecommendationUpdatedDescription(value) :
+                setRecommendationUpdatedDetails(value)
+    }
+
     return (
-        <Paper elevation={3} sx={{p: 3, mt: 3, height: '100%'}}>
-            <Grid container spacing={2}>
-                <Grid item xs={12} md={8}>
-                    <Typography variant="h5" sx={{color: theme.palette.primary.main, fontWeight: 500}}>
-                        Cluster Profile
-                    </Typography>
-                    <Typography variant="body1" mt={1} mb={3}>
-                        {cluster.assigned_cluster_profile.long_description}
-                    </Typography>
+        <>
 
-                    <Typography variant="h5" sx={{color: theme.palette.primary.main, fontWeight: 500}}>
-                        Suitable Recommendation
-                    </Typography>
-                    <Typography variant="body1" mt={1}>
-                        {cluster.assigned_cluster_profile.recommendation.description || 'Empty.'}
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} md={4} sx={{minHeight: '300px'}}>
-                    <LineChart actualLoad={cluster.assigned_cluster_profile.cluster[0].line_data} forecastedLoad={cluster.forecasted_load}/>
-                </Grid>
-            </Grid>
-        </Paper>
+            <Modal
+                open={editModal}
+                onClose={handleCloseModal}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography variant={'h5'} color={theme.palette.primary.main}>Editing Recommendation</Typography>
+                    <hr/>
+                    <Grid container spacing={2} mt={1} mb={3}>
+                        <Grid item xs={12}>
+                            <TextField
+                                onChange={e => handleFieldChange('name', e.target.value)}
+                                required
+                                fullWidth
+                                id="outlined-required"
+                                label="Change Recommendation name"
+                                value={recommendationUpdatedName}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                onChange={e => handleFieldChange('description', e.target.value)}
+                                required
+                                fullWidth
+                                id="outlined-required"
+                                label="Change Recommendation description"
+                                value={recommendationUpdatedDescription}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                onChange={e => handleFieldChange('details', e.target.value)}
+                                required
+                                fullWidth
+                                id="outlined-required"
+                                label="Change Recommendation details"
+                                value={recommendationUpdatedDetails}
+                            />
+                        </Grid>
+                    </Grid>
+                    <Stack direction={'row'} sx={{mt: 2}}>
+                        <Button variant="outlined" color="error" onClick={handleCloseModal} sx={{mx: 1, ml: 'auto'}}>
+                            DISCARD
+                        </Button>
+                        <Button variant="contained" color="success"
+                                disabled={recommendationUpdatedName === '' || recommendationUpdatedDescription === '' || recommendationUpdatedDetails === ''}
+                                onClick={() =>
+                                    handleSaveRecommendation(recommendationUpdatedName, recommendationUpdatedDescription, recommendationUpdatedDetails)}
+                        >
+                            SAVE
+                        </Button>
+                    </Stack>
+                </Box>
+            </Modal>
 
+
+            <Paper elevation={3} sx={{p: 3, mt: 3, height: '100%'}}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={8}>
+                        <Typography variant="h5" sx={{color: theme.palette.primary.main, fontWeight: 500}}>
+                            Cluster Profile
+                        </Typography>
+                        <Typography variant="body1" mt={1} mb={3}>
+                            {cluster.assigned_cluster_profile.long_description}
+                        </Typography>
+
+                        <Typography variant="h5">
+                            <span style={{
+                                color: theme.palette.primary.main,
+                                fontWeight: 500
+                            }}>Suitable Recommendation</span>: {cluster.assigned_cluster_profile.recommendation.name}
+                        </Typography>
+                        <Typography variant="body1" mt={1}>
+                            {cluster.assigned_cluster_profile.recommendation.description} ({cluster.assigned_cluster_profile.recommendation.details})
+                        </Typography>
+                        <Button size={'medium'} variant="outlined" color={'warning'} sx={{my: 2}}
+                                startIcon={<EditNoteIcon/>}
+                                onClick={() => handleOpenModal(cluster.assigned_cluster_profile.recommendation)}
+                        >
+                            Edit Recommendation
+                        </Button>
+                    </Grid>
+                    <Grid item xs={12} md={4} sx={{minHeight: '300px'}}>
+                        <LineChart actualLoad={cluster.assigned_cluster_profile.cluster[0].line_data}
+                                   forecastedLoad={cluster.forecasted_load}/>
+                    </Grid>
+                </Grid>
+            </Paper>
+        </>
     );
 }
 
--- src/pages/SmartMetersInspection.js
+++ src/pages/SmartMetersInspection.js
@@ -9,6 +9,7 @@ import Breadcrumb from "../components/layout/Breadcrumb";
 import MLModel from "../components/SmartMetersInspection/MLModel";
 import SmartMeter from "../components/SmartMetersInspection/SmartMeter";
 import Data from "../components/SmartMetersInspection/Data";
+import AlertCustom from "../components/layout/AlertCustom";
 
 const SmartMetersInspection = () => {
     const {id} = useParams()
@@ -27,31 +28,73 @@ const SmartMetersInspection = () => {
             fontWeight={600}>
             {`Smart Meter #${id}`}
         </Typography>,];
-    const [details, setDetails] = useState('')
+    const [smartMeterDetails, setSmartMeterDetails] = useState('')
+
+
+    // Variables for modal and for edit message snackbars
+    const [editModal, setEditModal] = useState(false)
+    const [editSuccess, setEditSuccess] = useState(false)
+    const [editFailure, setEditFailure] = useState(false)
 
     // TODO ml_model_id to be deleted in next steps
     useEffect(() => {
         axios.get(`/assignments/${id}/64fed83517072e1bdd31f0ed`)
             .then(response => {
-                setDetails(response.data)
+                setSmartMeterDetails(response.data)
+                console.log(response.data)
             })
             .catch(error => {
                 console.log(error)
             })
     }, [])
 
+    const handleSaveRecommendation = (name, description, details) => {
+        const payload = {
+            recommendation: {
+                name, description, details
+            }
+        }
+
+        axios.put(`/assignments/edit/${smartMeterDetails.id}`, payload)
+            .then(response => {
+                setEditSuccess(true)
+                setSmartMeterDetails(response.data)
+                setEditModal(false)
+            })
+            .catch(() => {
+                setEditFailure(true)
+                setEditModal(false)
+            })
+    }
+
+    const handleCloseSnackbarEdit = () => {
+        setEditSuccess(false)
+        setEditFailure(false)
+    }
+
     return (
         <>
             <Breadcrumb breadcrumbs={breadcrumbs} welcome_msg={''}/>
 
             <Container maxWidth={false} sx={{my: 5}}>
-                {details &&
+                {smartMeterDetails &&
                     <>
-                        <MLModel model={details.ml_model}/>
-                        <SmartMeter meter={details.meter}/>
-                        <Data cluster={details}/>
+                        <MLModel model={smartMeterDetails.ml_model}/>
+                        <SmartMeter meter={smartMeterDetails.meter}/>
+                        <Data cluster={smartMeterDetails} handleSaveRecommendation={handleSaveRecommendation}
+                              editModal={editModal}
+                              setEditModal={setEditModal}/>
                     </>}
             </Container>
+
+            {editSuccess &&
+                <AlertCustom open={editSuccess} actionClose={handleCloseSnackbarEdit} severity={'success'}
+                             message={'The recommendation has been successfully edited!'}/>}
+
+            {editFailure &&
+                <AlertCustom open={editFailure} actionClose={handleCloseSnackbarEdit} severity={'error'}
+                             message={'Oops! Something wrong happened. Please try again!'}/>}
+
         </>
     );
 }
