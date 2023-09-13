19344f663e60e47846fd505e85170be740a7830e
gkorbakis <gkorbakis@epu.ntua.gr>
2023-09-13 11:32:02 +0300

    Assignments.js: additions

--- src/pages/Assignments.js
+++ src/pages/Assignments.js
@@ -3,9 +3,19 @@ import {Link} from "react-router-dom";
 import axios from "axios";
 
 import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, {tableCellClasses} from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from "@mui/material/TableRow";
 
 import Breadcrumb from "../components/layout/Breadcrumb";
 import Loading from "../components/layout/Loading";
+import {StyledTableRow} from "../components/layout/TableComponents";
+import {StyledTableCell} from "../components/layout/TableComponents";
 
 const Assignments = () => {
     const breadcrumbs = [
@@ -22,6 +32,44 @@ const Assignments = () => {
     return (
         <>
             <Breadcrumb breadcrumbs={breadcrumbs} welcome_msg={''}/>
+            <Box sx={{padding: 3, maxWidth: "100vw"}}>
+                <Container maxWidth={false} sx={{my: 5, display: 'flex'}}>
+                    <Container maxWidth={false}>
+                        <Table size="small" aria-label="customized table">
+                            <TableHead>
+                                <TableRow>
+                                    <StyledTableCell>
+                                        <Typography fontWeight={'bold'} variant={'subtitle1'}>
+                                            Date
+                                        </Typography>
+                                    </StyledTableCell>
+                                    <StyledTableCell align="left">
+                                        <Typography fontWeight={'bold'} variant={'subtitle1'}>
+                                            Device ID
+                                        </Typography>
+                                    </StyledTableCell>
+                                    <StyledTableCell align="left">
+                                        <Typography fontWeight={'bold'} variant={'subtitle1'}>
+                                            Device Type
+                                        </Typography>
+                                    </StyledTableCell>
+                                    <StyledTableCell align="left">
+                                        <Typography fontWeight={'bold'} variant={'subtitle1'}>
+                                            Cluster
+                                        </Typography>
+                                    </StyledTableCell>
+                                    <StyledTableCell align="left">
+                                        <Typography fontWeight={'bold'} variant={'subtitle1'}>
+                                            Cluster assigned description
+                                        </Typography>
+                                    </StyledTableCell>
+                                    <StyledTableCell align="right">{void (0)}</StyledTableCell>
+                                </TableRow>
+                            </TableHead>
+                        </Table>
+                    </Container>
+                </Container>
+            </Box>
         </>
     );
 }
