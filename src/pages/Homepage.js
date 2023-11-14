45943d4a08ec4b265843e1ee1b29b9eb89792b99
gkorbakis <gkorbakis@epu.ntua.gr>
2023-11-14 18:50:59 +0200

    Homepage.test.js added / Homepage.js corresponding changes / package.json: small changes for compatibility

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,12 +3,14 @@ import {servicesHomepage} from "../components/Homepage/servicesHomepage";
 
 const Homepage = () => {
     return (
-        <>
+        <div data-testid={"homepageOverall"}>
             {servicesHomepage.map((service, index) => (
-                <HomepageItemFullWidth title={service.title} description={service.description} icon={service.icon}
-                                       image={service.image} link={service.link} index={index} key={service.id}/>
+                <div data-testid={"homepageItem"}>
+                    <HomepageItemFullWidth title={service.title} description={service.description} icon={service.icon}
+                                           image={service.image} link={service.link} index={index} key={service.id}/>
+                </div>
             ))}
-        </>
+        </div>
     );
 }
 
